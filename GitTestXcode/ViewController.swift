ac986923b51fe41caef7a74fa8903721f663ed68
Vladimir Bondarenko <vbembler@gmail.com>
2021-01-19 17:44:16 +0300

    Initial Commit

---
+++ GitTestXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestXcode
+//
+//  Created by Vladimir on 19/01/2021.
+//  Copyright © 2021 Embler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
