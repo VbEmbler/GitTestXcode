640442af9473df03523e1e5c7a010c0c7d4c74b1
Vladimir Bondarenko <vbembler@gmail.com>
2021-01-19 18:55:01 +0300

    Created new branch. Added greeting Label Two

--- GitTestXcode/ViewController.swift
+++ GitTestXcode/ViewController.swift
@@ -11,11 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTwo: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         labelOne.text = "Hello World"
+        labelTwo.text = "Hello New World"
     }
 
 
